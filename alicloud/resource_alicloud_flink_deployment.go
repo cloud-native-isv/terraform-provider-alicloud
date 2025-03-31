ac0a6a0d6776c2f8901736c18d3995a338c346bc
Qiming, Liu <liuqiming.lqm@alibaba-inc.com>
2025-04-01 00:48:59 +0800

    fix workspace

--- alicloud/resource_alicloud_flink_deployment.go
+++ alicloud/resource_alicloud_flink_deployment.go
@@ -1,8 +1,14 @@
 package alicloud
 
 import (
+	"fmt"
+	"log"
+	"time"
+
 	"github.com/aliyun/terraform-provider-alicloud/alicloud/connectivity"
+	"github.com/hashicorp/terraform-plugin-sdk/helper/resource"
 	"github.com/hashicorp/terraform-plugin-sdk/helper/schema"
+	foasconsole "github.com/alibabacloud-go/foasconsole-20211028/client"
 )
 
 func resourceAliCloudFlinkDeployment() *schema.Resource {
@@ -36,27 +42,329 @@ func resourceAliCloudFlinkDeployment() *schema.Resource {
 			"parallelism": {
 				Type:     schema.TypeInt,
 				Optional: true,
+				Default:  1,
 			},
+			"jar_uri": {
+				Type:     schema.TypeString,
+				Required: true,
+			},
+			"jar_artifact_name": {
+				Type:     schema.TypeString,
+				Optional: true,
+				Computed: true,
+			},
+			"program_args": {
+				Type:     schema.TypeString,
+				Optional: true,
+			},
+			"deployment_id": {
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+			"deployment_name": {
+				Type:     schema.TypeString,
+				Optional: true,
+				Computed: true,
+			},
+			"status": {
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+			"create_time": {
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+			"update_time": {
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+		},
+		Timeouts: &schema.ResourceTimeout{
+			Create: schema.DefaultTimeout(10 * time.Minute),
+			Update: schema.DefaultTimeout(10 * time.Minute),
+			Delete: schema.DefaultTimeout(5 * time.Minute),
 		},
 	}
 }
 
 func resourceAliCloudFlinkDeploymentCreate(d *schema.ResourceData, meta interface{}) error {
 	client := meta.(*connectivity.AliyunClient)
-	return fmt.Errorf("not implemented")
+	foasService := FoasService{client}
+
+	workspaceId := d.Get("workspace_id").(string)
+	namespace := d.Get("namespace").(string)
+	jobName := d.Get("job_name").(string)
+
+	request := foasconsole.CreateDeployJobRequest()
+	request.WorkspaceId = workspaceId
+	request.Namespace = namespace
+	request.JobName = jobName
+	request.EntryClass = d.Get("entry_class").(string)
+	request.JarUri = d.Get("jar_uri").(string)
+
+	if v, ok := d.GetOk("parallelism"); ok {
+		request.Parallelism = v.(int)
+	}
+
+	if v, ok := d.GetOk("jar_artifact_name"); ok {
+		request.JarArtifactName = v.(string)
+	}
+
+	if v, ok := d.GetOk("program_args"); ok {
+		request.ProgramArgs = v.(string)
+	}
+
+	if v, ok := d.GetOk("deployment_name"); ok {
+		request.DeploymentName = v.(string)
+	} else {
+		// Generate a default deployment name if not provided
+		request.DeploymentName = fmt.Sprintf("%s-%s", jobName, time.Now().Format("20060102150405"))
+	}
+
+	var response *foasconsole.DeployJobResponse
+	var err error
+
+	err = resource.Retry(d.Timeout(schema.TimeoutCreate), func() *resource.RetryError {
+		raw, err := foasService.DeployJob(request)
+		if err != nil {
+			if IsExpectedErrors(err, []string{"OperationFailed", "ServiceUnavailable"}) {
+				return resource.RetryableError(err)
+			}
+			return resource.NonRetryableError(err)
+		}
+		response = raw
+		return nil
+	})
+	
+	if err != nil {
+		return WrapErrorf(err, DefaultErrorMsg, "alicloud_flink_deployment", request.GetActionName(), AlibabaCloudSdkGoERROR)
+	}
+
+	d.SetId(fmt.Sprintf("%s:%s:%s", workspaceId, namespace, response.DeploymentId))
+	d.Set("deployment_id", response.DeploymentId)
+
+	// Wait for the deployment to be running
+	stateConf := BuildStateConf([]string{"CREATING", "STARTING"}, []string{"RUNNING"}, d.Timeout(schema.TimeoutCreate), 5*time.Second, foasService.FlinkDeploymentStateRefreshFunc(d.Id()))
+	if _, err := stateConf.WaitForState(); err != nil {
+		return WrapErrorf(err, IdMsg, d.Id())
+	}
+
+	return resourceAliCloudFlinkDeploymentRead(d, meta)
 }
 
 func resourceAliCloudFlinkDeploymentRead(d *schema.ResourceData, meta interface{}) error {
 	client := meta.(*connectivity.AliyunClient)
-	return fmt.Errorf("not implemented")
+	foasService := FoasService{client}
+
+	parts, err := ParseResourceId(d.Id(), 3)
+	if err != nil {
+		return WrapError(err)
+	}
+	workspaceId := parts[0]
+	namespace := parts[1]
+	deploymentId := parts[2]
+
+	request := foasconsole.CreateGetDeploymentRequest()
+	request.WorkspaceId = workspaceId
+	request.Namespace = namespace
+	request.DeploymentId = deploymentId
+
+	var deployment *foasconsole.GetDeploymentResponseDeployment
+	err = resource.Retry(5*time.Minute, func() *resource.RetryError {
+		raw, err := foasService.GetDeployment(request)
+		if err != nil {
+			if IsExpectedErrors(err, []string{"ResourceNotFound", "ServiceUnavailable"}) {
+				if NotFoundError(err) {
+					log.Printf("[DEBUG] Resource alicloud_flink_deployment foasService.GetDeployment Failed!!! %s", err)
+					d.SetId("")
+					return nil
+				}
+				return resource.RetryableError(err)
+			}
+			return resource.NonRetryableError(err)
+		}
+
+		deployment = raw.Deployment
+		return nil
+	})
+
+	if err != nil {
+		return WrapErrorf(err, DefaultErrorMsg, d.Id(), request.GetActionName(), AlibabaCloudSdkGoERROR)
+	}
+
+	if deployment == nil {
+		d.SetId("")
+		return nil
+	}
+
+	d.Set("workspace_id", workspaceId)
+	d.Set("namespace", namespace)
+	d.Set("deployment_id", deploymentId)
+	d.Set("job_name", deployment.JobName)
+	d.Set("deployment_name", deployment.DeploymentName)
+	d.Set("entry_class", deployment.EntryClass)
+	d.Set("parallelism", deployment.Parallelism)
+	d.Set("jar_uri", deployment.JarUri)
+	d.Set("jar_artifact_name", deployment.JarArtifactName)
+	d.Set("program_args", deployment.ProgramArgs)
+	d.Set("status", deployment.Status)
+	d.Set("create_time", deployment.CreateTime)
+	d.Set("update_time", deployment.UpdateTime)
+
+	return nil
 }
 
 func resourceAliCloudFlinkDeploymentUpdate(d *schema.ResourceData, meta interface{}) error {
 	client := meta.(*connectivity.AliyunClient)
-	return fmt.Errorf("not implemented")
+	foasService := FoasService{client}
+	
+	parts, err := ParseResourceId(d.Id(), 3)
+	if err != nil {
+		return WrapError(err)
+	}
+	workspaceId := parts[0]
+	namespace := parts[1]
+	deploymentId := parts[2]
+
+	d.Partial(true)
+	
+	update := false
+	request := foasconsole.CreateUpdateDeploymentRequest()
+	request.WorkspaceId = workspaceId
+	request.Namespace = namespace
+	request.DeploymentId = deploymentId
+
+	if d.HasChange("job_name") {
+		request.JobName = d.Get("job_name").(string)
+		update = true
+	}
+
+	if d.HasChange("entry_class") {
+		request.EntryClass = d.Get("entry_class").(string)
+		update = true
+	}
+
+	if d.HasChange("parallelism") {
+		request.Parallelism = d.Get("parallelism").(int)
+		update = true
+	}
+
+	if d.HasChange("jar_uri") {
+		request.JarUri = d.Get("jar_uri").(string)
+		update = true
+	}
+
+	if d.HasChange("jar_artifact_name") {
+		request.JarArtifactName = d.Get("jar_artifact_name").(string)
+		update = true
+	}
+
+	if d.HasChange("program_args") {
+		request.ProgramArgs = d.Get("program_args").(string)
+		update = true
+	}
+
+	if d.HasChange("deployment_name") {
+		request.DeploymentName = d.Get("deployment_name").(string)
+		update = true
+	}
+
+	if update {
+		var response *foasconsole.UpdateDeploymentResponse
+		var err error
+		
+		err = resource.Retry(d.Timeout(schema.TimeoutUpdate), func() *resource.RetryError {
+			raw, err := foasService.UpdateDeployment(request)
+			if err != nil {
+				if IsExpectedErrors(err, []string{"OperationFailed", "ServiceUnavailable"}) {
+					return resource.RetryableError(err)
+				}
+				return resource.NonRetryableError(err)
+			}
+			response = raw
+			return nil
+		})
+		
+		if err != nil {
+			return WrapErrorf(err, DefaultErrorMsg, d.Id(), request.GetActionName(), AlibabaCloudSdkGoERROR)
+		}
+
+		stateConf := BuildStateConf([]string{"UPDATING"}, []string{"RUNNING"}, d.Timeout(schema.TimeoutUpdate), 5*time.Second, foasService.FlinkDeploymentStateRefreshFunc(d.Id()))
+		if _, err := stateConf.WaitForState(); err != nil {
+			return WrapErrorf(err, IdMsg, d.Id())
+		}
+
+		if d.HasChange("job_name") {
+			d.SetPartial("job_name")
+		}
+		if d.HasChange("entry_class") {
+			d.SetPartial("entry_class")
+		}
+		if d.HasChange("parallelism") {
+			d.SetPartial("parallelism")
+		}
+		if d.HasChange("jar_uri") {
+			d.SetPartial("jar_uri")
+		}
+		if d.HasChange("jar_artifact_name") {
+			d.SetPartial("jar_artifact_name")
+		}
+		if d.HasChange("program_args") {
+			d.SetPartial("program_args")
+		}
+		if d.HasChange("deployment_name") {
+			d.SetPartial("deployment_name")
+		}
+	}
+
+	d.Partial(false)
+	return resourceAliCloudFlinkDeploymentRead(d, meta)
 }
 
 func resourceAliCloudFlinkDeploymentDelete(d *schema.ResourceData, meta interface{}) error {
 	client := meta.(*connectivity.AliyunClient)
-	return fmt.Errorf("not implemented")
+	foasService := FoasService{client}
+
+	parts, err := ParseResourceId(d.Id(), 3)
+	if err != nil {
+		return WrapError(err)
+	}
+	workspaceId := parts[0]
+	namespace := parts[1]
+	deploymentId := parts[2]
+
+	request := foasconsole.CreateDeleteDeploymentRequest()
+	request.WorkspaceId = workspaceId
+	request.Namespace = namespace
+	request.DeploymentId = deploymentId
+
+	var response *foasconsole.DeleteDeploymentResponse
+	err = resource.Retry(d.Timeout(schema.TimeoutDelete), func() *resource.RetryError {
+		raw, err := foasService.DeleteDeployment(request)
+		if err != nil {
+			if IsExpectedErrors(err, []string{"OperationFailed", "ServiceUnavailable"}) {
+				return resource.RetryableError(err)
+			}
+			return resource.NonRetryableError(err)
+		}
+		response = raw
+		return nil
+	})
+
+	if err != nil {
+		if IsExpectedErrors(err, []string{"ResourceNotFound"}) {
+			return nil
+		}
+		return WrapErrorf(err, DefaultErrorMsg, d.Id(), request.GetActionName(), AlibabaCloudSdkGoERROR)
+	}
+
+	stateConf := BuildStateConf([]string{"DELETING", "STOPPING"}, []string{}, d.Timeout(schema.TimeoutDelete), 5*time.Second, foasService.FlinkDeploymentStateRefreshFunc(d.Id()))
+	if _, err := stateConf.WaitForState(); err != nil {
+		if NotFoundError(err) {
+			return nil
+		}
+		return WrapErrorf(err, IdMsg, d.Id())
+	}
+
+	return nil
 }
--- alicloud/resource_alicloud_flink_workspace.go
+++ alicloud/resource_alicloud_flink_workspace.go
@@ -1,14 +1,14 @@
 package alicloud
 
 import (
-	"fmt"
 	"log"
 	"time"
 
+	foasconsole "github.com/alibabacloud-go/foasconsole-20211028/client"
+	"github.com/alibabacloud-go/tea/tea"
 	"github.com/aliyun/terraform-provider-alicloud/alicloud/connectivity"
 	"github.com/hashicorp/terraform-plugin-sdk/helper/resource"
 	"github.com/hashicorp/terraform-plugin-sdk/helper/schema"
-	foasconsole "github.com/alibabacloud-go/foasconsoleClient-20211028/client"
 )
 
 func resourceAliCloudFlinkWorkspace() *schema.Resource {
@@ -30,6 +30,21 @@ func resourceAliCloudFlinkWorkspace() *schema.Resource {
 				Type:     schema.TypeString,
 				Optional: true,
 			},
+			"resource_group_id": {
+				Type:     schema.TypeString,
+				Required: true,
+				ForceNew: true,
+			},
+			"zone_id": {
+				Type:     schema.TypeString,
+				Required: true,
+				ForceNew: true,
+			},
+			"oss_bucket": {
+				Type:     schema.TypeString,
+				Required: true,
+				ForceNew: true,
+			},
 			"vpc_id": {
 				Type:     schema.TypeString,
 				Required: true,
@@ -40,6 +55,119 @@ func resourceAliCloudFlinkWorkspace() *schema.Resource {
 				Required: true,
 				Elem:     &schema.Schema{Type: schema.TypeString},
 				ForceNew: true,
+			},
+			"cpu": {
+				Type:     schema.TypeInt,
+				Optional: true,
+				Default:  10000,
+			},
+			"memory": {
+				Type:     schema.TypeInt,
+				Optional: true,
+				Default:  40000,
+			},
+			"instance_type": {
+				Type:     schema.TypeString,
+				Optional: true,
+			},
+			"tags": {
+				Type:     schema.TypeMap,
+				Optional: true,
+				Elem:     &schema.Schema{Type: schema.TypeString},
+			},
+			"security_group_id": {
+				Type:     schema.TypeString,
+				Optional: true,
+			},
+			"architecture_type": {
+				Type:     schema.TypeString,
+				Optional: true,
+			},
+			"auto_renew": {
+				Type:     schema.TypeBool,
+				Optional: true,
+			},
+			"charge_type": {
+				Type:     schema.TypeString,
+				Required: true,
+			},
+			"duration": {
+				Type:     schema.TypeInt,
+				Optional: true,
+			},
+			"extra": {
+				Type:     schema.TypeString,
+				Optional: true,
+			},
+			"ha": {
+				Type:     schema.TypeBool,
+				Optional: true,
+			},
+			"ha_resource_spec": {
+				Type:     schema.TypeList, // Need detailed spec struct handling
+				Optional: true,
+				MaxItems: 1,
+				Elem: &schema.Resource{
+					Schema: map[string]*schema.Schema{
+						// Define fields here based on HaResourceSpec struct
+					},
+				},
+			},
+			"ha_vswitch_ids": {
+				Type:     schema.TypeList,
+				Optional: true,
+				Elem:     &schema.Schema{Type: schema.TypeString},
+			},
+			"ha_zone_id": {
+				Type:     schema.TypeString,
+				Optional: true,
+			},
+			"monitor_type": {
+				Type:     schema.TypeString,
+				Optional: true,
+			},
+			"pricing_cycle": {
+				Type:     schema.TypeString,
+				Optional: true,
+			},
+			"promotion_code": {
+				Type:     schema.TypeString,
+				Optional: true,
+			},
+			"region": {
+				Type:     schema.TypeString,
+				Required: true,
+			},
+			"resource_spec": {
+				Type:     schema.TypeList, // For ResourceSpec struct
+				Required: true,
+				MaxItems: 1,
+				Elem: &schema.Resource{
+					Schema: map[string]*schema.Schema{
+						"cpu": {
+							Type:     schema.TypeInt,
+							Required: true,
+						},
+						"memory": {
+							Type:     schema.TypeInt,
+							Required: true,
+						},
+					},
+				},
+			},
+			"storage": {
+				Type:     schema.TypeList, // For Storage struct
+				Required: true,
+				MaxItems: 1,
+				Elem: &schema.Resource{
+					Schema: map[string]*schema.Schema{
+						// Define storage fields here
+					},
+				},
+			},
+			"use_promotion_code": {
+				Type:     schema.TypeBool,
+				Optional: true,
 			},
 		},
 		Timeouts: &schema.ResourceTimeout{
@@ -54,18 +182,74 @@ func resourceAliCloudFlinkWorkspaceCreate(d *schema.ResourceData, meta interface
 	client := meta.(*connectivity.AliyunClient)
 	foasService := FoasService{client}
 
-	request := foasconsole.CreateCreateInstanceRequest()
-	request.Name = d.Get("name").(string)
-	request.VpcId = d.Get("vpc_id").(string)
-	request.VSwitchIds = d.Get("vswitch_ids").([]string)
-	request.Description = d.Get("description").(string)
+	request := foasconsole.CreateInstanceRequest{}
 
-	response, err := foasService.CreateInstance(request)
+	// Populate mandatory fields
+	request.InstanceName = tea.String(d.Get("name").(string))
+	request.ResourceGroupId = tea.String(d.Get("resource_group_id").(string))
+	request.VpcId = tea.String(d.Get("vpc_id").(string))
+	request.ZoneId = tea.String(d.Get("zone_id").(string))
+
+	ossBucket := d.Get("oss_bucket").(string) // 提取OSS bucket值到storage结构
+	request.Storage = &foasconsole.CreateInstanceRequestStorage{
+		Oss: &foasconsole.CreateInstanceRequestStorageOss{
+			Bucket: tea.String(ossBucket),
+		},
+	}
+
+	// Handle vswitch_ids as list
+	vswitchIds := d.Get("vswitch_ids").([]interface{})
+	request.VSwitchIds = make([]*string, 0, len(vswitchIds))
+	for _, v := range vswitchIds {
+		request.VSwitchIds = append(request.VSwitchIds, tea.String(v.(string)))
+	}
+
+	// Handle nested ResourceSpec
+	resourceSpec := d.Get("resource_spec").([]interface{})[0].(map[string]interface{})
+	request.ResourceSpec = &foasconsole.CreateInstanceRequestResourceSpec{
+		Cpu:     tea.Int32(int32(resourceSpec["cpu"].(int))),
+		MemoryGB:  tea.Int32(int32(resourceSpec["memory"].(int))),
+	}
+
+	// Handle boolean fields with proper conversion
+	request.AutoRenew = tea.Bool(d.Get("auto_renew").(bool))
+	request.Ha = tea.Bool(d.Get("ha").(bool))
+	request.UsePromotionCode = tea.Bool(d.Get("use_promotion_code").(bool))
+
+	// Handle optional fields
+	if val, ok := d.GetOk("duration"); ok {
+		request.Duration = tea.Int32(int32(val.(int)))
+	}
+
+	if val, ok := d.GetOk("promotion_code"); ok {
+		request.PromotionCode = tea.String(val.(string))
+	}
+
+	tags := d.Get("tags").(map[string]interface{})
+	if len(tags) > 0 {
+		request.Tag = make([]*foasconsole.CreateInstanceRequestTag, 0, len(tags))
+		for k, v := range tags {
+			tag := &foasconsole.CreateInstanceRequestTag{
+				Key:   tea.String(k),
+				Value: tea.String(v.(string)),
+			}
+			request.Tag = append(request.Tag, tag)
+		}
+	}
+
+	request.ChargeType = tea.String(d.Get("charge_type").(string))
+	request.Region = tea.String(d.Get("region").(string))
+
+	response, err := foasService.CreateInstance(&request)
 	if err != nil {
 		return WrapError(err)
 	}
 
-	d.SetId(response.InstanceId)
+	if response.Success == nil || !*response.Success {
+		return WrapErrorf(Error("CreateInstance failed: %s", response.Message))
+	}
+
+	d.SetId(*response.Body.OrderInfo.InstanceId)
 
 	stateConf := BuildStateConf([]string{"CREATING"}, []string{"RUNNING"}, d.Timeout(schema.TimeoutCreate), 10*time.Second, foasService.FlinkWorkspaceStateRefreshFunc(d.Id()))
 	if _, err := stateConf.WaitForState(); err != nil {
