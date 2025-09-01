8e88c0dd752abc52fc6b603479b8e8684f5ff847
Qiming Liu (John) <john.liuqiming@linux.alibaba.com>
2025-09-01 16:21:30 +0800

    update terraform provider code

--- alicloud/provider.go
+++ alicloud/provider.go
@@ -987,7 +987,6 @@ func Provider() terraform.ResourceProvider {
 			"alicloud_apig_http_api":                                         resourceAliCloudApigHttpApi(),
 			"alicloud_arms_alert_contact":                                    resourceAliCloudArmsAlertContact(),
 			"alicloud_arms_alert_contact_group":                              resourceAliCloudArmsAlertContactGroup(),
-
 			"alicloud_arms_alert_integration":                                resourceAliCloudArmsAlertIntegration(),
 			"alicloud_arms_alert_notification_policy":                        resourceAliCloudArmsAlertNotificationPolicy(),
 			"alicloud_arms_alert_robot":                                      resourceAliCloudArmsAlertRobot(),
